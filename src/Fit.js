946baae8dea190dcc7a482f3db985e057f62ded8
ash1day <y4ashida@gmail.com>
2016-11-02 10:26:01 +0900

    適合度指標表示するものを絞った

--- src/Fit.js
+++ src/Fit.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import './Fit.css'
 
+const fitsKeyNames = [ 'gfi', 'agfi', 'rmr', 'cfi', 'aic', 'rmsea' ]
+
 class Fit extends Component {
   componentDidMount() {
     this.fits = this.props.json.goodness_of_fit
@@ -16,9 +18,10 @@ class Fit extends Component {
     if (!this.fits) return <div className="Fit" />
 
     for (const k of Object.keys(this.fits).sort()) {
+      if (fitsKeyNames.includes(k))
       rows.push(
         <tr key={k} >
-          <th>{k}</th>
+          <th>{k.toUpperCase()}</th>
           <td>{this.fits[k].split('.')[0]}</td>
           <td>.{this.fits[k].split('.')[1]}</td>
         </tr>
